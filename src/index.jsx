45416f6cd3ee68a863d53f6f68f81028c3d995bc
Mahdi Najjarian <mnajarian@gmail.com>
2019-03-18 17:50:26 +0200

    first commit

--- src/index.jsx
+++ src/index.jsx
@@ -7,14 +7,41 @@ import './assets/stylesheets/style.css'
 
 /****** ADD YOUR CODE AFTER THIS LINE ******/
 
-const Hello = () => (
-  <div>Hello Docker!</div>
-);
+const Hello = (props) => {
+  const icon = `http://openweathermap.org/img/w/${props.icon}.png`
+  return(
+    <div>
+      <h2>Helsinki Weather</h2>
+      <h3>{props.description}</h3>
+      <img src={icon} height='200px' alt="weather-icon"/>
+    </div>
+  )
+}
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      weather: []
+    }
+  }
+  componentDidMount() {
+    fetch('https://weatherapp.eficode.fi/api/forecast')
+    .then(response => {
+      return response.json()
+    })
+    .then((obj) => {
+      this.setState({
+        weather: obj
+      })
+    })
+  }
   render() {
+
+console.log(this.state.weather)
     return (
-      <Hello />
+      <Hello icon={this.state.weather.icon}
+        description={this.state.weather.description}/>
     );
   }
 }
